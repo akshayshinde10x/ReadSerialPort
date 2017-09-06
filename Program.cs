5c7c412f02830c5e0cdb6bf08413283fd776f673
Akshay Shinde <akshayshinde281195@gmail.com>
2017-09-06 15:18:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort mySerialPort = new SerialPort("COM3");
+
+            mySerialPort.BaudRate = 115200;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            mySerialPort.Open();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.ReadKey();
+            mySerialPort.Close();
+        }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Data Received:");
+            Console.WriteLine(indata);
+
+            File.WriteAllText("text_file.json",indata);
+            Thread.Sleep(100);
+        }
+      
+           
+    }
+}
